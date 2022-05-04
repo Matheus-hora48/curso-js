5fef26649e36f528cd29b44f5c9826b8a91e54c2
Matheus Hora <matheus11hora@gmail.com>
2022-05-04 17:39:03 -0300

    Mudanças

--- projetoagenda/src/models/ContatoModel.js
+++ projetoagenda/src/models/ContatoModel.js
@@ -3,9 +3,8 @@ const validator = require('validator');
 
 const ContatoSchema = new mongoose.Schema({
   nome: { type: String, required: true },
-  sobrenome: { type: String, required: false, default: '' },
-  email: { type: String, required: false, default: '' },
   telefone: { type: String, required: false, default: '' },
+  tipo: { type: String, required: true, default: '' },
   criadoEm: { type: Date, default: Date.now },
 });
 
@@ -28,11 +27,7 @@ Contato.prototype.valida = function() {
 
   // Validação
   // O e-mail precisa ser válido
-  if(this.body.email && !validator.isEmail(this.body.email)) this.errors.push('E-mail inválido');
   if(!this.body.nome) this.errors.push('Nome é um campo obrigatório.');
-  if(!this.body.email && !this.body.telefone) {
-    this.errors.push('Pelo menos um contato precisa ser enviado: e-mail ou telefone.');
-  }
 };
 
 Contato.prototype.cleanUp = function() {
@@ -44,9 +39,8 @@ Contato.prototype.cleanUp = function() {
 
   this.body = {
     nome: this.body.nome,
-    sobrenome: this.body.sobrenome,
-    email: this.body.email,
     telefone: this.body.telefone,
+    tipo: this.body.tipo,
   };
 };
 
